3e38f9784e446fc07c25a3201a6429f2bc7086d3
Roobubba <roobubba@gmail.com>
2022-07-05 16:24:22 +0100

    Rocket weapon and projectile created, including radial falloff damage

--- Source/Blaster/BlasterTypes/WeaponType.h
+++ Source/Blaster/BlasterTypes/WeaponType.h
@@ -6,6 +6,6 @@ enum class EWeaponType : uint8
     EWT_AssaultRifle UMETA(DisplayName = "Assault Rifle"),
     EWT_Pistol UMETA(DisplayName = "Pistol"),
     EWT_Grenade UMETA(DisplayName = "Grenade"),
-    
+    EWT_RocketLauncher UMETA(DisplayName = "Rocket Launcher"),
     EWT_MAX UMETA(DisplayName = "DefaultMAX")
 };
