be3c51aaff847888a9577f7d7b848c1d6f6ceed3
Roobubba <roobubba@gmail.com>
2022-07-18 11:36:50 +0100

    Added respawning pickups and set up map

--- Source/Blaster/Pickups/Pickup.cpp
+++ Source/Blaster/Pickups/Pickup.cpp
@@ -34,9 +34,14 @@ void APickup::BeginPlay()
 	
 	if (HasAuthority() && OverlapSphere)
 	{
-		OverlapSphere->OnComponentBeginOverlap.AddDynamic(this, &APickup::OnSphereOverlap);
+		GetWorldTimerManager().SetTimer
+		(
+			BindOverlapTimer,
+			this,
+			&APickup::BindOverlapTimerFinished,
+			BindOverlapTime
+		);
 	}
-	
 }
 
 void APickup::Destroyed()
@@ -78,4 +83,9 @@ void APickup::Tick(float DeltaTime)
 void APickup::OnSphereOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor,	UPrimitiveComponent* OtherComp,	int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult)
 {
 
+}
+
+void APickup::BindOverlapTimerFinished()
+{
+	OverlapSphere->OnComponentBeginOverlap.AddDynamic(this, &APickup::OnSphereOverlap);
 }
